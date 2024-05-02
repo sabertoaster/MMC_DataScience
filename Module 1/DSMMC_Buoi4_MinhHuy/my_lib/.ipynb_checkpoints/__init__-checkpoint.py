9bb7db2d844014261473e843bea62ce05305675c
sabertoaster <62453787+sabertoaster@users.noreply.github.com>
2024-05-02 20:24:56 +0700

    Module 2 : Lab Session 01

---
+++ Module 1/DSMMC_Buoi4_MinhHuy/my_lib/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,7 @@
+from .XuLyFile import *
+from .MyPath import *
+from .XuLyTaiKhoan import *
+from .ThongKe import *
+from .Menu import *
+from .LoDeHoc import *
+
---
+++ Module 2/Sale_Management.sql
@@ -0,0 +1,11 @@
+USE sale_management;
+
+CREATE TABLE Customers (
+    customer_id INT, first_name VARCHAR(50), date_of_birth DATE
+);
+
+CREATE TABLE SALES (
+    customer_id INT, first_name VARCHAR(50), date_of_birth DATE
+);
+
+SHOW TABLES;
